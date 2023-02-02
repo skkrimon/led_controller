791f156b9e32c541f9b165548f6f98dc1c8e77c8
skkrimon <75689426+skkrimon@users.noreply.github.com>
2023-02-02 20:09:34 +0100

    Update basic.py

--- programs/basic.py
+++ programs/basic.py
@@ -1,10 +1,12 @@
 from rpi_ws281x import Color
+from time import sleep
 
 
 def static(strip, r, g, b, delay_ms=50):
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, Color(r, g, b))
-        strip.show(delay_ms / 1000.0)
+        strip.show()
+        sleep(delay_ms / 1000.0)
         
         
 def clear(strip):
